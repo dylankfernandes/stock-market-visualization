9e355826f7e476fb3db9a1150fac0bad84934de2
Dylan Fernandes <dylan.fernandes00@gmail.com>
2018-10-06 10:35:58 -0400

    switching to graphing branch

--- main.py
+++ main.py
@@ -9,13 +9,22 @@ import urllib
 
 style.use('fivethirtyeight')
 
+# Get formatted url for query
 def get_query(api_key, stock, interval='monthly'): 
   return 'https://www.alphavantage.co/query?function=TIME_SERIES_' + interval.upper() + '&symbol=MSFT&apikey=' + api_key + '&datatype=csv'
 
 def get_stock_data(stock):
+  # Read api key and use in query
   api_key = open('../api-key-alpha-vantage.txt', 'r').read()
   stock_url = get_query(api_key, stock)
   source_code = urllib.request.urlopen(stock_url).read().decode()
-  print(source_code)
+
+  # Turn query into useable data
+  stock_data = []
+  split_source_code = source_code.split('\n')
+  
+  for line in split_source_code:
+    split_line = line.split(',')
+    if len(split_line) == 
 
 get_stock_data('TSLA')
