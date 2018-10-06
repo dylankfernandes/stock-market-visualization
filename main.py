9b141e3278ea37932cc12d7ac8f526d48362d16e
Dylan Fernandes <dylan.fernandes00@gmail.com>
2018-10-06 10:21:19 -0400

    switching to api retrieval branch

---
+++ main.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.ticker as mticker
+from matplotlib import style
+
+import numpy as np
+import datetime as dt
+import urllib
+
+style.use('fivethirtyeight')
+
